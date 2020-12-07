92373d1b084d081c469c2368ef31c734fc3ef200
Teis Lindemark <teis.lindemark@miles.no>
2020-12-07 18:01:33 +0100

    Day 7

---
+++ Day07/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static bool isTreeSymmetric(char[][] forrest, int startX, int startY)
+    {
+        var isSymmetric = true;
+
+        for(var i=startY;i>=0;i--)
+        {
+            var j = 0;
+            while((-1*j)+startX > 0 && (forrest[i][(-1*j)+startX] != ' ' || forrest[i][j+startX] != ' '))
+            {
+                if((-1*j)+startX >= 0 && forrest[i][(-1*j)+startX] != forrest[i][j+startX]) return false;
+                j++;
+            }
+        }
+
+        return isSymmetric;
+    }
+
+    static void Main(string[] args)
+    {
+        char[][] forrest = File
+            .ReadLines(@"input.txt")
+            .Select(line => line.ToCharArray())
+            .ToArray();
+
+        var forrestBottom = forrest.Length-1;
+        var numSymmetricTrees = 0;
+        var numTrees = 0;
+
+        var x = 0;
+        foreach(var tree in forrest[forrestBottom])
+        {
+            if(tree == '#')
+            {
+                if(isTreeSymmetric(forrest, x, forrestBottom)) numSymmetricTrees++;
+                numTrees++;
+            }
+            x++;
+        }
+
+        Console.WriteLine(numSymmetricTrees);
+        Console.WriteLine(numTrees);
+    }
+}
---
+++ Day07/README.md
@@ -0,0 +1,5 @@
+Juletrebonden Eilert har ein skog med mange juletre. Diverre har skogen i år blitt angrepen av ein sopp som får trea til vekse skeivt, mangle greiner og generelt bli usymmetriske (om stamme-aksen). Usymmetriske tre blir ikkje kjøpt, så Eilert lurer på kor mange av trea han får seld i år.
+
+[Oversikt over alle trea finn du her](./forest.txt) (skru av word wrap / linjebryting!). Kor mange av trea er symmetriske om stammen?
+
+Vi ser her at det første treet er symmetrisk og fint. Som markert med piler manglar det andre ei grein og det tredje har skeiv stamme. Det siste treet manglar to greiner, men er framleis symmetrisk. Det er med andre ord kun det første og siste som kan selgast.
